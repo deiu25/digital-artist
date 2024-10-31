94044f063fe20dfcd6262306aac2fcdf00acf014
Badulescu Andrei <d3iu25@gmail.com>
2024-10-31 20:12:37 +0200

    frontend ok

--- client/src/components/ProjectsPageCard.jsx
+++ client/src/components/ProjectsPageCard.jsx
@@ -1,41 +1,35 @@
-const ProjectsPageCard = ({ date, category, title, description, imageUrl }) => {
+const ProjectsPageCard = ({ date, category, title, description, imageUrl, clientUrl, isVisible }) => {
+  if (!isVisible) return null; 
+
   return (
-    <div className="border border-transparent hover:border-orange-500 transition duration-200 rounded-2xl h-full p-3 group">
-      <div className="relative h-[240px]">
-        <div className="absolute top-0 left-0 z-10">
-          <svg className="rounded-tl-2xl" width="159" height="156">
-            <use xlinkHref="/images/icons/shapes.svg#shape-top-left" />
-          </svg>
+    <a href={clientUrl} target="_blank" rel="noopener noreferrer">
+      <div className="bg-p7 border border-transparent hover:border-orange-500 transition duration-200 rounded-2xl h-full p-3 group">
+        <div className="relative bg-white rounded-32 h-[240px]">
+          <img
+            className="absolute inset-0 w-full h-full object-cover rounded-2xl"
+            src={imageUrl}
+            alt=""
+          />
         </div>
-        <div className="absolute bottom-0 right-0 z-10">
-          <svg className="rounded-br-2xl" width="150" height="158">
-            <use xlinkHref="/images/icons/shapes.svg#shape-bottom-right" />
-          </svg>
-        </div>
-        <img
-          className="absolute inset-0 w-full h-full object-cover rounded-2xl"
-          src={imageUrl}
-          alt=""
-        />
-      </div>
-      <div className="py-6">
-        <div className="flex flex-wrap items-center gap-3 mb-3">
-          <p className="text-w1 group-hover:text-p1 transition duration-200 text-sm">
-            {date}
-          </p>
-          <div className="w-1 h-1 bg-[#B46995] rounded-full"></div>
-          <div className="py-1 px-2 rounded-md bg-white border border-gray-100 text-xs font-medium text-gray-700">
-            {category}
+        <div className="py-6">
+          <div className="flex flex-wrap items-center gap-3 mb-3">
+            <p className="text-w1 group-hover:text-p1 transition duration-200 text-sm">
+              {date}
+            </p>
+            <div className="w-1 h-1 bg-[#B46995] rounded-full"></div>
+            <div className="py-1 px-2 rounded-md bg-white border border-gray-100 text-xs font-medium text-gray-700">
+              {category}
+            </div>
           </div>
+          <h2 className="text-w1 group-hover: transition duration-200 text-xl font-bold font-angel mb-3 max-w-xs">
+            {title}
+          </h2>
+          <p className="text-w1 group-hover:text-p1 transition duration-200 font-satoshi text-sm">
+            {description}
+          </p>
         </div>
-        <h2 className="text-w1 group-hover: transition duration-200 text-xl font-bold font-angel mb-3 max-w-xs">
-          {title}
-        </h2>
-        <p className="text-w1 group-hover:text-p1 transition duration-200 font-satoshi text-sm">
-          {description}
-        </p>
       </div>
-    </div>
+    </a>
   );
 };
 
--- client/src/pages/AllProjects.jsx
+++ client/src/pages/AllProjects.jsx
@@ -1,24 +1,25 @@
 import HeaderProj from "../components/HeaderProj";
 import ProjectsPageCard from "../components/ProjectsPageCard";
+import { allprojectsData } from "../constants";
 
 const AllProjects = () => {
   return (
     <section className="overflow-hidden">
-      <div className="bg-p7 pt-40">
+      <div className="sm:bg-hero-section mt-40 px-[12%]">
         <div className="container px-4 mx-auto">
           <h1 className="text-w1 text-4xl lg:text-6xl font-bold font-angel text-center mb-6 pt-16">
             My Projects
           </h1>
-          <p className="text-w1 text-xl text-center font-satoshi mb-20">
+          <p className="text-w1 text-xl text-center mb-20">
             Discover the latest projects, tips and user stories insights from
             Solstice.
           </p>
           <div className="pb-20 mb-20 border-b border-p1">
             <HeaderProj
-              date="20 Jul 2023"
-              category="Education"
-              title="How to Maintain Good Wellbeing While Studying"
-              description="Here are some tips on how to maintain good wellbeing while studying."
+              date="20 Dec 2024"
+              category="News"
+              title="New Techniques to Enhance Tattoo Art and Client Experience"
+              description="Discover the latest techniques in tattoo artistry to elevate design quality and improve client comfort."
               imageUrl="/images/about-art-tatoo.png"
             />
           </div>
@@ -27,17 +28,19 @@ const AllProjects = () => {
           </h2>
 
           <div className="flex flex-wrap mb-8 pb-8 border-b border-p1 -mx-4">
-            <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-              <a href="#">
+            {allprojectsData.map((project, index) => (
+              <div key={index} className="w-full md:w-1/2 lg:w-1/3 p-4">
                 <ProjectsPageCard
-                  date="8 Jul 2023"
-                  category="Tips"
-                  title="Make Your Phone Your Best Study Tool"
-                  description="How do we make our phone best study tool? Read on to find out more."
-                  imageUrl="solstice-assets/images/blog/picture1.png"
+                  date={project.date}
+                  category={project.category}
+                  title={project.title}
+                  description={project.description}
+                  imageUrl={project.imageUrl}
+                  clientUrl={project.clientUrl}
+                  isVisible={project.isVisible}
                 />
-              </a>
-            </div>
+              </div>
+            ))}
           </div>
           <div className="flex items-center justify-between flex-wrap gap-4 pb-32">
             <p className="text-w1 text-sm">
@@ -50,16 +53,10 @@ const AllProjects = () => {
               <span className="font-semibold">10</span>
             </p>
             <div className="flex flex-wrap gap-3">
-              <a
-                className="next-prev"
-                href="#"
-              >
+              <a className="next-prev" href="#">
                 Prev
               </a>
-              <a
-                className="next-prev"
-                href="#"
-              >
+              <a className="next-prev" href="#">
                 Next
               </a>
             </div>
